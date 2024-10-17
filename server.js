fac7070fa6d54298ca6522bd399001dba894a7ab
RohanKumar63 <rohankumar9415369@gmail.com>
2024-10-17 22:59:27 +0530

    api created

--- server.js
+++ server.js
@@ -15,12 +15,11 @@ app.use(express.json());
 app.use('/api/comics', comicBookRoutes);
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.log('MongoDB connection error:', err));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.log('MongoDB connection error:', err));
+
 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
